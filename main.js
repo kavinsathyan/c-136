ac78399f382d4b4bf2e5dc01cb0fd65d8c39483b
kavinsathyan <79074982+kavinsathyan@users.noreply.github.com>
2021-10-08 15:32:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+video = "";
+status = "";
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 300);
+    canvas.center();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
